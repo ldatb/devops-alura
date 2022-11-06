2b2eb25d39e319bb9f4d6f7ad8b60cdc681d0e5d
Lucas de Ataides <lucasatab@gmail.com>
2022-11-06 13:58:09 -0300

    Updated Instance name

--- main.tf
+++ main.tf
@@ -14,11 +14,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "learning-terraform" {
-  ami = "ami-00266f51b6b22db58"
+resource "aws_instance" "learning-devops-alura" {
+  ami = "ami-09a41e26df464c548"
   instance_type = "t2.micro"
+  key_name = "learning-terraform"
+
+  #  user_data = <<-EOF
+  #    #!/bin/bash
+  #    cd ~
+  #    sudo apt-get update && sudo apt-get -y install busybox
+  #    echo "<h1>Ola mundo</h1><h2>Feito com o Terraform</h2>" > index.html
+  #    nohup busybox httpd -f -p 8080 &
+  #  EOF
 
   tags = {
-    Name = "Aprendendo-Terraform"
+    Name = "DevOps-Alura-AWS"
   }
 }
